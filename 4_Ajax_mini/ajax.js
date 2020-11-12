46d9a5594c9a4a5616cb7614e25398fdb33a28d1
Art Lees <art.lees@dragonpeace.com>
2020-11-12 08:38:57 -0500

    201112 AM1 push

---
+++ 4_Ajax_mini/ajax.js
@@ -0,0 +1,22 @@
+const ajax = (url, callback, method = "GET") => {
+    if (!url) return console.error("Request Required");
+    if (!callback) return console.error("Callback Required");
+    const request = new XMLHttpRequest();
+    request.addEventListener("readystatechange", (evt) => {
+        let req = evt.target;
+        if (req.readyState !== 4) return;
+        if (req.status === 200) return callback(req.response);
+        callback("");
+        });
+    request.open(method, url);
+    request.send();
+    });
+
+    let value = (res, input) => document.body.append(res);
+    ajax("https://randomuser.me/api/", value);
+
+    src="data.json"
+
+    ajax("data.json", (r)=>{
+        console.log(JSON.parse(r))
+    })
